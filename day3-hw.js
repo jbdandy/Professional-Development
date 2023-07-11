eab9a09bbe3521b735df763b2088931efd2112f6
Jeremy Dandy <jeremydmarsreturnship.com>
2023-07-10 19:50:33 -0500

    day3 homework update 2

--- day3-hw.js
+++ day3-hw.js
@@ -1,26 +1,27 @@
-const findMinimum = (listOfNumbers)=>{
+const findMinimum = (listOfNumbers) => {
     //if no numbers in list return -1;
 
-    if (listOfNumbers === 0);{
+    if (listOfNumbers.length === 0) {
         return -1;
     }
     //otherwise return the smallest number
     let min = 0;
-    for (let i = 0; i < listOfNumbers.length; i++){
+
+    for (let i = 0; i < listOfNumbers.length;i++) { //check each number in list,  //is current number less than the minimum
         const currentOne = listOfNumbers[i];
-        console.log (currentOne);
+        console.log(currentOne);
         if (currentOne < min) {
-            min = currentOne;
+            min = currentOne;    //yes: update minimum,    //no: do nothing}
         }
     }
     return min;
-
-    //check each number in list
-
-        //is current number less than the minimum
-        //yes: update minimum
-        //no: do nothing
 }
+
+    
+
+   
+   
+ 
 
 /*
 example 1:
@@ -39,20 +40,20 @@ output: 1
 
 //Paste your solution into here to test it: https://leetcode.com/problems/two-sum/
 //Given a list are there 2 numbers that add to x; if there is two numbers that add to x return true, otherwise false
-const twoSum = (listOfNumbers, x)=>{
+const twoSum = (listOfNumbers, x) => {
     let result = false;
 
 
 
     //check every combination of numbers to see if the add up to x
     //hint: need two loops, start one at the first element and the other at the second element in list
-    
-    //loop 1 - start at first element in list
-        const currentValueA = ?;//first item in list
-        //loop 2 - start at second in list
-            const currentValueB = ?;//second item in list
 
-            //if current value A plus current value B equal x return true otherwise keep going
+    //loop 1 - start at first element in list
+    const currentValueA = ?;//first item in list
+    //loop 2 - start at second in list
+    const currentValueB = ?;//second item in list
+
+    //if current value A plus current value B equal x return true otherwise keep going
 
 
     return result;
@@ -61,7 +62,7 @@ const twoSum = (listOfNumbers, x)=>{
 //returns the number of words in the sentence
 //input: "happy birthday jane"
 //output: 3
-const countNumberOfWords = (sentence)=>{
+const countNumberOfWords = (sentence) => {
     //how do you determine where one word starts and the other ends?
     //hint: you will have to check every character in the sentence
 
@@ -70,7 +71,7 @@ const countNumberOfWords = (sentence)=>{
 //hints:
 //1 - use what you know from countNumberOfWords problem
 //2 - keep track of length of each would
-//3 - to track length use a new variable that is a counter, rest counter once you hit the next word, 
+//3 - to track length use a new variable that is a counter, rest counter once you hit the next word,
 //at end of the sentence your count will be the length of the last word
 
 
