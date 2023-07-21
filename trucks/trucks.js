a2e876cbf5c4a8632babbf5f57a50ed880e090d3
Jeremy Dandy <jeremydmarsreturnship.com>
2023-07-20 19:11:30 -0500

    trucks

--- trucks/trucks.js
+++ trucks/trucks.js
@@ -9,5 +9,5 @@ const howManyTrucks
 1. How many total trips?
 2. How many trips originated in PDX
 3. How many trips had a destination of SLC and trip length over 300 miles.
-
+4. How many trips went from XXX to YYY - pick two destinations
 */
