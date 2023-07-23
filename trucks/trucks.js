d8f304420a947a776bdd2242693a86f6047d0b5b
Jeremy Dandy <jeremydmarsreturnship.com>
2023-07-23 18:36:39 -0500

    trucks update1

--- trucks/trucks.js
+++ trucks/trucks.js
@@ -3,11 +3,19 @@ const trip = data.truckloads;
 
 
 // determine how many trucks
-const howManyTrucks
+const howManyTrucks = (allTrucks);{
+    let count = 0;
 
-/*
-1. How many total trips?
-2. How many trips originated in PDX
-3. How many trips had a destination of SLC and trip length over 300 miles.
-4. How many trips went from XXX to YYY - pick two destinations
-*/
+    for (i < 0; i < howManyTrucks.length; i++) {
+        const truckLog = allTrucks.length[i];
+        if truckLog = 
+    }
+}
+
+
+//1. How many total trips?
+
+
+//2. How many trips originated in PDX
+//3. How many trips had a destination of SLC and trip length over 300 miles.
+//4. How many trips went from XXX to YYY - pick two destinations
