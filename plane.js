35a068918560a23749764dd0dbc78b7f53235cf0
Jeremy Dandy <jeremydmarsreturnship.com>
2023-08-01 19:01:23 -0500

    Plane HW

---
+++ plane.js
@@ -0,0 +1,4 @@
+//Write a program to create a class called Plane with seats, status reflecting if all passengers have been served
+//Seat contains properties status (whether or not was served), order information (what drink and food), name (ex: 14A)
+//Program should take all seats orders and then delivery food/drink to each seat, print out "Complete" when the whole plane 
+//has been served. print out each seats order once it was served "Delivered: <food> <drink> to <seat name>"
--- tokens/tokens.js
+++ tokens/tokens.js
@@ -1,13 +1,13 @@
 //Venmo - dollars
 
-class Person{
-    firstName='';
-    lastName='';
-    email='';
-    phone='';
-    usdBalance=0
+class Person {
+    firstName = '';
+    lastName = '';
+    email = '';
+    phone = '';
+    usdBalance = 0
 
-    constructor(firstName,lastName,email,phone,usdBalance){
+    constructor(firstName, lastName, email, phone, usdBalance) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.email = email;
@@ -16,69 +16,54 @@ class Person{
     }
 }
 
-class Transactions{
+class Transaction {
     toPersonName;
     fromPersonName;
     amount;
     memo;
     date;
-    constructor( toPersonName, fromPersonName, amount, memo, date){
-            //TODO: map the default values, see other constructors
-            this.toPersonName = toPersonName;
-            this.fromPersonName = fromPersonName;
-            this.amount = amount;
-            this.memo = memo;
-            this.date = date;
-        }
+    constructor(toPersonName, fromPersonName, amount, memo, date) {
+        //map the default values, see other constructors
+        this.toPersonName = toPersonName;
+        this.fromPersonName = fromPersonName;
+        this.amount = amount;
+        this.memo = memo;
+        this.date = date;
+    }
 }
 
-class Bank{
+class Bank {
     transactions = [];
 
-    depositUsd(amount, person){
-        //TODO
-        //how many you want - amount
-        //whos buying them - person
-        if(amount <= firstName.lastName.usdBalance){
-            usdBalance = usdBalance + amount;
-        }else{
-            console.log("Insufficient Funds")
+    //infinite money from somewhere to deposit into bank account
+    depositUsd(amount, person) {
+        //TODO: update person's usd balance with the new balance based on the amount deposited
+        if (amount >= 0) {
+            person.usdBalance = person.usdBalance + amount;
+
+            //add transaction to the bank instance
+            const randomIncomeEmployer = new Person('random', 'income-source', 'random@test.com', '', 1000000000);
+            const newTransaction = new Transaction(randomIncomeEmployer.firstName, person.firstName, amount, 'deposit from random income source', new Date())
+            this.transactions.push(newTransaction);
         }
     }
-        //Do they have enough money to buy it
-        //if they have enough money give them the tokens and updated the usdBalance
-        //if they don't have money - console log 'insufficient funds'
 
 
-
-    
-    //TODO: create new transaction
-    
-    //const newTransaction = new Transaction('jane','joe',4,'lost bet','12/15/23')
-   //this.bank.transactions.push(newTransaction);
-    
-
-    const newTransaction = new Transaction(fromPerson, toPerson, amount, memo, date)
-    bank.transactions.push('jane', 'joe', 4, 'lost bet', '12/15/23')
-
-
-    newTransaction(fromPerson, toPerson, amount, memo, date){
-
-    }
-    //see line 112
-    
-        
-
-
-
-    sendUsd(amount, fromPerson, toPerson, memo){
+    sendUsd(amount, fromPerson, toPerson, memo) {
         //TODO
         //does the person sending tokens have enough tokens to send
-        if(amount <= fromPerson.usdBalance){
+        if (amount <= fromPerson.usdBalance) {
             //if they do update from person token balance and to person token balance
-            toPerson.usdbalance == toPerson.usdBalance + amount && fromPerson.usdBalance == fromPerson.usdBalance - amount;
+            toPerson.usdBalance = toPerson.usdBalance + amount;
+            fromPerson.usdBalance = fromPerson.usdBalance - amount;
 
-        }else{ //console log 'insufficent token balance'
+            //TODO: create a transaction and store it in the banks transactions array
+
+            const newTransaction = new Transaction(toPerson.firstName, fromPerson.firstName, 200, "sending you money", new Date("08/01/2013"))
+            this.transactions.push(newTransaction);
+
+
+        } else { //console log 'insufficent token balance'
             console.log("Insufficient Token Balance")
         }
 
@@ -87,31 +72,35 @@ class Bank{
 }
 
 
-const main = ()=>{
+const main = () => {
 
 
-    const janeSmith = new Person('Jane','Smith', 'jsmith@pretendmail.com', 5555555555,100)
-    const jonDoe = new Person('Jon', 'Doe', 'jd@pretendemail.com', 7777777777,10)
+    const janeSmith = new Person('Jane', 'Smith', 'jsmith@pretendmail.com', 5555555555, 100)
+    const jonDoe = new Person('Jon', 'Doe', 'jd@pretendemail.com', 7777777777, 10)
     console.log(janeSmith.usdBalance);
 
-    const bank = new Bank();
+    const associatedBank = new Bank();
+    const usBank = new Bank();
 
-    //Jane wants to add 3 gold tokens
-    bank.depositUsd(3, janeSmith);
+    //Jane 
+    associatedBank.depositUsd(3, janeSmith);
+    usBank.depositUsd(2, janeSmith);
+    //Joe
+    associatedBank.depositUsd(4, jonDoe);
 
-    //Jane wants to add 2 silver tokens
-    bank.depositUsd(2, janeSmith);
-    //Joe wants to add 4 diamond tokens
-    bank.depositUsd(4, jonDoe);
+    //Jane sends Jon 5
+    associatedBank.sendUsd(5, janeSmith, jonDoe, 'lost bet');
 
-    //Jane sends Jon 5 gold tokens
-    bank.sendUsd( 5, janeSmith, jonDoe, 'lost bet');
+    //Jane sends Jon 1 
+    associatedBank.sendUsd(1, janeSmith, jonDoe, 'burger');
 
-    //Jane sends Jon 1 gold token
-    bank.sendUsd(1, janeSmith, jonDoe, 'burger');
+    console.log(associatedBank.transactions);
 
-    //bank.newTransaction('jane', 'joe', 4, 'lost bet', '12/15/23');
-
-    console.log(bank.transactions);
+    console.log(`Expect Janes associatedBank and usBank balance to be: 99, actual:${janeSmith.usdBalance} and Jons to be: 20 actual: ${jonDoe.usdBalance}`);
+    console.log(`Expect associatedBank to have 4 number of transactions, actual:${associatedBank.transactions.length}`);
+    console.log(`Expect usBank to have 1 number of transactions, actual:${usBank.transactions.length}`);
+    console.log(`Expect usBank to have correct number of transactions: ${usBank.transactions.length === 1 ? 'PASS':'FAIL'}`);
+    //testing frameworks like jest that use code that looks like this
+    // expect(usBank.transactions.length).toEqual(1);
 }
 main();
