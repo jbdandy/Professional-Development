04452584c46c71aa37d765bbcdcf6d30fa7dc2a0
Jeremy Dandy <jeremydmarsreturnship.com>
2023-08-03 17:06:24 -0500

    plane 8/3

--- plane.js
+++ plane.js
@@ -2,3 +2,21 @@
 //Seat contains properties status (whether or not was served), order information (what drink and food), name (ex: 14A)
 //Program should take all seats orders and then delivery food/drink to each seat, print out "Complete" when the whole plane 
 //has been served. print out each seats order once it was served "Delivered: <food> <drink> to <seat name>"
+
+
+//creat class called plane with seats
+class planeWithSeats {
+    served = '';
+    orderInfo = '';
+    seatNumber = '';
+
+
+
+    constructor(served, orderInfo, seatNumber) {
+        this.served = served;
+        this.orderInfo = orderInfo;
+        this.seatNumber = seatNumber;
+    }
+}
+
+//
