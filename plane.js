360ab5bcf89e06260fbc59341369741bed02de10
Jeremy Dandy <jeremydmarsreturnship.com>
2023-08-03 18:34:04 -0500

    plane 8/1 final

--- plane.js
+++ plane.js
@@ -5,44 +5,111 @@
 
 
 //creat class called plane with seats
-class planeWithSeats {
-    served = '';
-    notServed = '';
-    drinkFood = ''
-    seatNumber = '';
+class PlaneOrderingSystem {
+    orders = [];//array of type Order
 
 
-
-    constructor(served, notServed, drinkFood, seatNumber) {
-        this.served = served;
-        this.notServed = notServed;
-        this.drinkFood = drinkFood;
-        this.seatNumber = seatNumber;
+    //return true or false if all orders were served
+    getServingStatus(){
+        //TODO:calculate if all orders have been served
+        
     }
-}
 
-// class takeOrder {
-//     food = '';
-//     drink = '';
+    //returns number of orders left to serve
+    howManyLeftToServe(){
+        //TODO: have a list of N orders; How do you know if a single order was not served? How would you count this for all orders?
+        return this.orders.length - howManyOrdersWereServed()
+    }
 
-//     constructor(food, drink){
-//         this.food = food;
-//         this.drink = drink;
-//     }
-// }
+    //returns number of orders served
+    howManyOrdersWereServed(){
+        //TODO: have a list of N orders; How do you know if a single order was served? How would you count this for all orders?
+        const countOfOrdersServed = 0;    //served
+        // const ordersWithoutXs = 0; //not served
+        
+        //go one by one to see if order has an X on it
+        for (let i = 0; i < this.orders.length; i++){
+            //if it does then put in X pile otherwise put in other pile
+            const currentOrder = this.orders[i];//{food:'banana',drink:'coke',served:false,seatNumber:'1A'}
+            if(currentOrder.served === true){
+                countOfOrdersServed = countOfOrdersServed + 1;
+                //served
+            }
+            // else{
+            //     ordersWithoutXs = ordersWithoutXs + 1;
+            //     //not served
+                
+            // }
 
-//take all seat orders
-class seatOrders {
-
-    orders = [];
-
-    takeOrder (food, drink, seatNumber) {
-        if (drink = 1) {
+            //if the current order is served add it to the ordersWithXs array
+            
+            //else add it to the ordersWithoutXs
 
         }
+        //TODO:
+
+    }
+
+    //can only serve at most 5 orders at a time
+    serveOrders(){
+        //TODO:
+    }
+
+    //newOrder is an order object (instance of a class)
+    addOrder(newOrder){
+        //Adds a newOrder to the orders array; add item to an array. what is the item? what is the array?
+        this.orders.push(newOrder); //push() will add item to existing array
     }
 }
+
+//Tracking orders not seats
+class Order {
+    food = '';
+    drink = '';
+    served=false;
+    seatNumber='';//need so we know where to deliver it
+
+    //calling constructor method looks like `new Order("banana","coke","11A")
+    constructor(food, drink, seatNumber){ //constructor is a fancy keyword that creates a block in the computers memory to store data
+        this.food = food; //sub-block, aka property; set it equal to what the caller told me to - 'banana'
+        this.drink = drink;
+        this.seatNumber = seatNumber;
+    }
+
+    //wasServed:boolean
+    updateServedStatus(wasServed){
+        this.served = wasServed;
+    }
+}
+
+
 
 //deliver all seat orders
 
 //print "Complete" when entire plan has been served
+
+
+const main = ()=>{
+
+    //initiate Plane
+
+    //create a bunch of orders
+    const order1 = new Order('banana','coke','1A');//creates order
+    order1.updateServedStatus(true);//updates status
+
+    //serve all orders
+
+    //check status of plane
+
+}
+
+const ex1 = ()=>{
+    const count = 0;
+    count = count + 1;
+}
+console.log(ex1());//?
+
+const ex2 = ()=>{
+    return 100;
+}
+console.log(ex2());//?
